2fb02b6ceb3751f6157fe58dc00ea1cfdd8be0ae
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-06-09 13:50:34 +0300

    Add default api controller & use mvc

--- source/src/TouristAttractions/Startup.cs
+++ source/src/TouristAttractions/Startup.cs
@@ -17,6 +17,8 @@ namespace TouristAttractions
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +51,8 @@ namespace TouristAttractions
                 // Don't expose file system
                 EnableDirectoryBrowsing = false
             });
+
+            app.UseMvc();
         }
     }
 }
