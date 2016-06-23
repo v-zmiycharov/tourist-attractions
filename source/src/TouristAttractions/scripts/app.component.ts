7fc504b3b43226714f0e32073ca2407bdf691c4a
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-06-23 16:58:02 +0300

    Add app-header and app-footer components

--- source/src/TouristAttractions/scripts/app.component.ts
+++ source/src/TouristAttractions/scripts/app.component.ts
@@ -1,5 +1,7 @@
 ﻿import { Component }       from '@angular/core';
 import { AttractionService }     from './attraction.service';
+import { AppHeaderComponent } from './app-header.component';
+import { AppFooterComponent } from './app-footer.component';
 import { AttractionsComponent } from './attractions.component';
 import { DashboardComponent } from './dashboard.component';
 import { AttractionDetailComponent } from './attraction-detail.component';
@@ -8,7 +10,11 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
 @Component({
     selector: 'my-app',
     templateUrl: 'appTemplates/app.component.html',
-    directives: [ROUTER_DIRECTIVES],
+    directives: [
+        ROUTER_DIRECTIVES,
+        AppHeaderComponent,
+        AppFooterComponent
+    ],
     providers: [
         ROUTER_PROVIDERS,
         AttractionService
@@ -34,6 +40,4 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
     },
 ])
 
-export class AppComponent {
-    title = 'Tour of Attractions';
-}
+export class AppComponent {}
---
+++ source/src/TouristAttractions/wwwroot/appScripts/app-footer.component.js
@@ -0,0 +1,25 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var AppFooterComponent = (function () {
+    function AppFooterComponent() {
+    }
+    AppFooterComponent = __decorate([
+        core_1.Component({
+            selector: 'app-footer',
+            templateUrl: 'appTemplates/app-footer.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppFooterComponent);
+    return AppFooterComponent;
+}());
+exports.AppFooterComponent = AppFooterComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWZvb3Rlci5jb21wb25lbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zY3JpcHRzL2FwcC1mb290ZXIuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQyxxQkFBMEIsZUFBZSxDQUFDLENBQUE7QUFPM0M7SUFBQTtJQUFrQyxDQUFDO0lBTG5DO1FBQUMsZ0JBQVMsQ0FBQztZQUNQLFFBQVEsRUFBRSxZQUFZO1lBQ3RCLFdBQVcsRUFBRSx3Q0FBd0M7U0FDeEQsQ0FBQzs7MEJBQUE7SUFFZ0MseUJBQUM7QUFBRCxDQUFDLEFBQW5DLElBQW1DO0FBQXRCLDBCQUFrQixxQkFBSSxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsi77u/aW1wb3J0IHsgQ29tcG9uZW50IH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XHJcblxyXG5AQ29tcG9uZW50KHtcclxuICAgIHNlbGVjdG9yOiAnYXBwLWZvb3RlcicsXHJcbiAgICB0ZW1wbGF0ZVVybDogJ2FwcFRlbXBsYXRlcy9hcHAtZm9vdGVyLmNvbXBvbmVudC5odG1sJ1xyXG59KVxyXG5cclxuZXhwb3J0IGNsYXNzIEFwcEZvb3RlckNvbXBvbmVudCB7IH1cclxuIl19
